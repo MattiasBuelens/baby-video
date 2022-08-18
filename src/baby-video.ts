655763207dc2905114c12abc9e0dfd04ca3a1597
Mattias Buelens <mattias@buelens.com>
2022-08-18 22:34:38 +0200

    Add a black canvas to the DOM

--- src/baby-video.ts
+++ src/baby-video.ts
@@ -1,6 +1,28 @@
+import stylesheet from "./style.css";
+
+const template = document.createElement("template");
+template.innerHTML = `<style>${stylesheet}</style>`;
+
 export class BabyVideoElement extends HTMLElement {
+  readonly #canvas: HTMLCanvasElement;
+  readonly #canvasContext: CanvasRenderingContext2D;
+
   constructor() {
     super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(template.content.cloneNode(true));
+
+    this.#canvas = document.createElement("canvas");
+    this.#canvas.width = 1920;
+    this.#canvas.height = 1080;
+    this.#canvas.style.width = "100%";
+    this.#canvas.style.aspectRatio = "16 / 9";
+    shadow.appendChild(this.#canvas);
+
+    this.#canvasContext = this.#canvas.getContext("2d")!;
+    this.#canvasContext.fillStyle = "black";
+    this.#canvasContext.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
   }
 }
 
