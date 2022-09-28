6971a86079e060d2da26e3373fe70c772bd5ffee
Mattias Buelens <mattias.buelens@theoplayer.com>
2022-09-28 17:28:21 +0200

    Build for demo

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // https://vitejs.dev/guide/build.html#library-mode
   build: {
-    lib: {
-      entry: resolve(__dirname, "./src/index.ts"),
-      name: "BabyVideo",
-      fileName: "baby-video",
-    },
+    target: "es2022",
   },
 });
