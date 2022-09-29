a1fd985f6b7763ae99978fbe869f513dfd98cd0f
Mattias Buelens <mattias.buelens@theoplayer.com>
2022-09-29 16:21:57 +0200

    Play the entirety of Big Buck Bunny in demo

--- demo/app.ts
+++ demo/app.ts
@@ -1,5 +1,6 @@
 import "media-chrome";
 import { BabyMediaSource, BabyVideoElement } from "../src/index";
+import { waitForEvent } from "../src/util";
 
 const video = document.querySelector<BabyVideoElement>("baby-video")!;
 video.addEventListener("loadedmetadata", logEvent);
@@ -17,12 +18,14 @@ video.addEventListener("seeked", logEvent);
 video.addEventListener("progress", logEvent);
 video.addEventListener("resize", logEvent);
 
+const streamDuration = 643.56;
+
 const mediaSource = new BabyMediaSource();
 video.srcObject = mediaSource;
 if (mediaSource.readyState !== "open") {
   await waitForEvent(mediaSource, "sourceopen");
 }
-mediaSource.duration = 60;
+mediaSource.duration = streamDuration;
 const sourceBuffer = mediaSource.addSourceBuffer(
   'video/mp4; codecs="avc1.640028"'
 );
@@ -41,11 +44,62 @@ for (const segmentURL of segmentURLs) {
   await waitForEvent(sourceBuffer, "updateend");
 }
 
-function waitForEvent(target: EventTarget, type: string): Promise<Event> {
-  return new Promise((resolve) => {
-    target.addEventListener(type, resolve, { once: true });
-  });
+interface Segment {
+  url: string;
+  isLast: boolean;
 }
+
+const segmentDuration = 4;
+
+function getSegmentForTime(time: number): Segment | undefined {
+  const segmentIndex = 1 + Math.floor(time / segmentDuration);
+  if (segmentIndex < 0) {
+    return undefined;
+  }
+  return {
+    url: `https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_1920x1080_8000k/bbb_30fps_1920x1080_8000k_${segmentIndex}.m4v`,
+    isLast: segmentIndex === 159,
+  };
+}
+
+let pendingBufferLoop: Promise<void> = Promise.resolve();
+
+async function bufferLoop(signal: AbortSignal) {
+  await pendingBufferLoop;
+  while (true) {
+    if (signal.aborted) throw signal.reason;
+    const currentTime = video.currentTime;
+    const currentRange = video.buffered.find(currentTime);
+    const targetTime = currentRange ? currentRange[1] : currentTime;
+    if (targetTime - currentTime < 20) {
+      const segment = getSegmentForTime(targetTime);
+      if (segment) {
+        const segmentData = await (
+          await fetch(segment.url, { signal })
+        ).arrayBuffer();
+        sourceBuffer.appendBuffer(segmentData);
+        await waitForEvent(sourceBuffer, "updateend");
+        if (segment.isLast) {
+          mediaSource.endOfStream();
+          break; // stop buffering until next seek
+        }
+        continue;
+      }
+    }
+    await waitForEvent(video, "timeupdate", signal);
+  }
+}
+
+let bufferAbortController: AbortController = new AbortController();
+
+function restartBuffering() {
+  bufferAbortController.abort();
+  bufferAbortController = new AbortController();
+  pendingBufferLoop = bufferLoop(bufferAbortController.signal).catch(() => {});
+}
+
+video.addEventListener("seeking", restartBuffering);
+restartBuffering();
 
 function logEvent(event: Event) {
   console.log(`${event.type} @ ${video.currentTime}`);
