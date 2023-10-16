8f4ae414391751c3cb8bc2913b83034ea5801bd7
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-10-16 14:47:35 +0200

    Play in single quality

--- demo/app.ts
+++ demo/app.ts
@@ -39,22 +39,10 @@ const videoSourceBuffer = mediaSource.addSourceBuffer(
 const audioSourceBuffer = mediaSource.addSourceBuffer(
   'audio/mp4; codecs="mp4a.40.5"'
 );
-const videoSegmentURLs = [
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_640x360_1000k/bbb_30fps_640x360_1000k_0.m4v",
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_640x360_1000k/bbb_30fps_640x360_1000k_1.m4v",
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_640x360_1000k/bbb_30fps_640x360_1000k_2.m4v",
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_1920x1080_8000k/bbb_30fps_1920x1080_8000k_0.m4v",
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_1920x1080_8000k/bbb_30fps_1920x1080_8000k_2.m4v",
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_1920x1080_8000k/bbb_30fps_1920x1080_8000k_3.m4v",
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_1920x1080_8000k/bbb_30fps_1920x1080_8000k_4.m4v"
-];
-const audioSegmentURLs = [
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_a64k/bbb_a64k_0.m4a",
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_a64k/bbb_a64k_1.m4a",
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_a64k/bbb_a64k_2.m4a",
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_a64k/bbb_a64k_3.m4a",
-  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_a64k/bbb_a64k_4.m4a"
-];
+const videoInitSegmentURL =
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_1920x1080_8000k/bbb_30fps_1920x1080_8000k_0.m4v";
+const audioInitSegmentURL =
+  "https://dash.akamaized.net/akamai/bbb_30fps/bbb_a64k/bbb_a64k_0.m4a";
 
 AbortSignal.prototype.throwIfAborted ??= function throwIfAborted(
   this: AbortSignal
@@ -74,8 +62,8 @@ async function appendSegments(
 }
 
 await Promise.all([
-  appendSegments(videoSourceBuffer, videoSegmentURLs),
-  appendSegments(audioSourceBuffer, audioSegmentURLs)
+  appendSegments(videoSourceBuffer, [videoInitSegmentURL]),
+  appendSegments(audioSourceBuffer, [audioInitSegmentURL])
 ]);
 
 interface Segment {
